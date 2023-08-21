186277386b69b61fa3c02bed2093c656fee854ca
DanielGros <danielmgros@gmail.com>
2023-08-21 00:03:02 -0500

    reorganize 7-get-width

--- 7-get-width/src/get_width.py
+++ 7-get-width/src/get_width.py
@@ -1,10 +1,19 @@
 import math
  
 # Function to find distance
-def distance(x1, y1, z1, x2, y2, z2):
+def euclidian_distance(x1, y1, z1, x2, y2, z2):
       
     d = math.sqrt(math.pow(x2 - x1, 2) +
                 math.pow(y2 - y1, 2) +
                 math.pow(z2 - z1, 2)* 1.0)
     print("Distance is ")
     print(d)
+
+
+def main():
+    print("start")
+
+
+
+if __name__ == "__main__":
+    main()
