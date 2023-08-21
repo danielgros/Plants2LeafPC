5224038d9f57be5e029e3d6104810454e8caf477
DanielGros <danielmgros@gmail.com>
2023-08-20 23:28:56 -0500

    1-get-pc/validation/1_pc_to_csv.py

--- 1-get-pc/validation/1_pc_to_csv.py
+++ 1-get-pc/validation/1_pc_to_csv.py
@@ -1,16 +1,26 @@
 import pickle
 import sys
+import re
 
-# filename = sys.argv[1]
-filename = '/hdd/gros2/maskrcnn/samples/senior_thesis_project/1-get-pc/data/sample-data/output/pc_2023-01-30_11.00.00_png_ultra.txt'
+txt_input_path = sys.argv[1]
 
-with open(filename, 'rb') as f:
+remove1 = re.finditer(r"/", txt_input_path)
+remove2 = re.search(r"\b.txt", txt_input_path)
+*_, last = remove1
+beginning = 0
+middle = last.span()[1]
+end = remove2.span()[0]
+svo_file_path = txt_input_path[beginning:middle]
+svo_file_name = txt_input_path[middle:end]
+
+output_path = svo_file_path + svo_file_name + ".csv"
+
+
+with open(txt_input_path, 'rb') as f:
     point_cloud = pickle.load(f)
 
-filename = '/hdd/gros2/maskrcnn/samples/senior_thesis_project/1-get-pc/data/sample-data/output/pc_2023-01-30_11.00.00_png_ultra.csv'
-
-f = open(filename, "w")
-f = open(filename, "a")
+f = open(output_path, "w")
+f = open(output_path, "a")
 f.write("x_coor,y_coor,z_coor,r,g,b,a\n")
 for point in point_cloud:
     x, y, z, r, g, b, a = point
