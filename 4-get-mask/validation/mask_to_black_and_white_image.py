b4cebfa5c6056d8fefae698f2e49976917626cf4
DanielGros <danielmgros@gmail.com>
2023-08-21 19:56:14 -0500

    4-get-mask/validation in progress

--- 4-get-mask/validation/mask_to_black_and_white_image.py
+++ 4-get-mask/validation/mask_to_black_and_white_image.py
@@ -2,26 +2,24 @@ import numpy as np
 from PIL import Image
 import pickle
 
-mask_path = './mask.txt'
+mask_path = '../data/validation/mask.txt'
 
-# Create a random true/false array of size 1024x1024x1
-# array = np.random.choice([True, False], size=(1024, 1024, 1))
 with open(mask_path, 'rb') as f:
-    mask = pickle.load(f)
+    masks = pickle.load(f)
 
+mask = masks[list(masks.keys())[0]]
 
+# Conver the list to a numpy array
 array = np.array(mask)
 
-print(array.shape)
-
 # Convert true to black and false to white
-# array = np.where(array, 0, 255)
+array = np.where(array, 0, 255)
 
-# # Reshape the array to remove singleton dimension
-# array = array.squeeze()
+# Reshape the array to remove singleton dimension
+array = array.squeeze()
 
-# # Convert the numpy array to a PIL image
-# image = Image.fromarray(array.astype('uint8'), 'L')  # 'L' mode for grayscale
+# Convert the numpy array to a PIL image
+image = Image.fromarray(array.astype('uint8'), 'L')  # 'L' mode for grayscale
 
-# # Save the image
-# image.save('output_image.png')
+# Save the image
+image.save('../data/validation/output/leaf_image_black_and_white.jpg')
--- 4-get-mask/validation/validate_get_mask.sh
+++ 4-get-mask/validation/validate_get_mask.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 
-
-
-# convert mask object to black and white image
+python3 "../src/get_mask.py" 
+python3 "./mask_to_color_image.py"
+python3 "./mask_to_black_and_white_image.py"
