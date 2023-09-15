607dc3d5d42cd79b263e2e414c55200a02fc211c
Daniel Gros <danielmgros@gmail.com>
2023-09-15 12:33:43 -0500

    point cloud to project image code edits

--- 05-scale-mask/src/1_point_cloud_to_projected_image.py
+++ 05-scale-mask/src/1_point_cloud_to_projected_image.py
@@ -3,21 +3,55 @@ import cv2
 import csv
 import math
 
-def anglesToIndex(x_degree, y_degree, width, height, max_x_degree, max_y_degree):
-    x_interval = max_x_degree*2/width
-    y_interval = max_y_degree*2/height
-    x_shifted = x_degree+max_x_degree
+def getMaxDegrees(reader):
+    max_x_degree, min_x_degree, max_y_degree, min_y_degree = -math.inf, math.inf, -math.inf, math.inf
+
+    first_line = True
+    for lines in reader:
+        if first_line:
+            first_line = False
+            continue
+        x_coord = float(lines[0])
+        y_coord = float(lines[1])
+        z_coord = float(lines[2])
+        r = int(lines[3])
+        g = int(lines[4])
+        b = int(lines[5])
+        a = int(lines[6])
+        x_degrees = math.degrees(math.atan(x_coord/z_coord))
+        y_degrees = math.degrees(math.atan(y_coord/z_coord))
+
+        if x_degrees > max_x_degree:
+            max_x_degree = x_degrees
+        if x_degrees < min_x_degree:
+            min_x_degree = x_degrees
+        if y_degrees > max_y_degree:
+            max_y_degree = y_degrees
+        if y_degrees < min_y_degree:
+            min_y_degree = y_degrees
+
+    return [max_x_degree, min_x_degree, max_y_degree, min_y_degree]
+
+def anglesToIndex(x_degree, y_degree, width, height, max_x_degree, min_x_degree, max_y_degree, min_y_degree):
+
+    x_interval = (max_x_degree+abs(min_x_degree))/width
+    y_interval = (max_y_degree+abs(min_y_degree))/height
+    x_shifted = x_degree+((max_x_degree+abs(min_x_degree))/2)
     if x_shifted < 0:
-        x_shifted = 0
-    y_shifted = y_degree+max_y_degree
+        print("x negative")
+        x_shifted = -1
+    y_shifted = y_degree+((max_y_degree+abs(min_y_degree))/2)
     if y_shifted < 0:
-        y_shifted = 0
+        print("y negative")
+        y_shifted = -1
     x_pos = (x_shifted)/x_interval
     y_pos = (y_shifted)/y_interval
     if x_pos > width-1:
-        x_pos = width-1
+        print("x too big")
+        x_pos = -1
     if y_pos > height-1:
-        y_pos = height-1
+        print("y too big")
+        y_pos = -1
     return int(x_pos),int(y_pos)
 
 def removeVerticalBlackLines(img):
@@ -74,8 +108,12 @@ def pointCloudToImage():
     overlap = np.zeros([height,width])
     with open('../data/pc_2023-01-31_08.00.00_png_ultra.csv', mode = 'r') as file:
         reader = csv.reader(file)
+        max_x_degree, min_x_degree, max_y_degree, min_y_degree = getMaxDegrees(reader)
+    with open('../data/pc_2023-01-31_08.00.00_png_ultra.csv', mode = 'r') as file:
+        reader = csv.reader(file)
         points = []
         dict = {}
+
         first_line = True
         for lines in reader:
             if first_line:
@@ -90,7 +128,9 @@ def pointCloudToImage():
             a = int(lines[6])
             x_degrees = math.degrees(math.atan(x_coord/z_coord))
             y_degrees = math.degrees(math.atan(y_coord/z_coord))
-            x_pos,y_pos = anglesToIndex(x_degrees, y_degrees, width, height, 45, 30)
+            x_pos,y_pos = anglesToIndex(x_degrees, y_degrees, width, height, max_x_degree, min_x_degree, max_y_degree, min_y_degree)
+            if x_pos == -1 or y_pos == -1:
+                continue
             img[y_pos,x_pos,0] += r
             img[y_pos,x_pos,1] += g
             img[y_pos,x_pos,2] += b
@@ -98,6 +138,7 @@ def pointCloudToImage():
     for r in range(0,height):
         for c in range(0,width):
             if overlap[r,c] > 1:
+                print("there's an overlap", r, c)
                 img[r,c,0] /= overlap[r,c]
                 img[r,c,1] /= overlap[r,c]
                 img[r,c,2] /= overlap[r,c]
