af415a4182535f8ed8d7464705f90ce56f4f1113
Daniel Gros <danielmgros@gmail.com>
2023-09-18 17:34:47 -0500

    05 debugging in progress

--- 05-scale-mask/src/scale_mask.py
+++ 05-scale-mask/src/scale_mask.py
@@ -2,26 +2,21 @@ import pickle
 import numpy as np
 from scipy.ndimage import zoom
 import sys
+import cv2
 
 def scale_mask(mask, new_height, new_width):
-    y, x, z = np.array(mask).shape
+    print(np.array(mask).shape)
+    height, width, _depth = np.array(mask).shape
 
     # Calculate scaling factors
-    y_scale = new_height / y
-    x_scale = new_width / x
+    height_scale = new_height / height
+    width_scale = new_width / width
 
     # Scale the mask using nearest-neighbor interpolation
-    scaled_mask = zoom(mask, (y_scale, x_scale, 1), order=0)
+    scaled_mask = zoom(mask, (height_scale, width_scale, 1), order=0, mode='nearest')
 
     # Convert the scaled mask to boolean values (True/False)
-    scaled_mask = scaled_mask >= 0.5
-
-    # Print the original and scaled masks
-    # print("Original Mask:")
-    # print(mask)
-
-    # print("\nScaled Mask:")
-    # print(scaled_mask)
+    scaled_mask = scaled_mask >= 0.9
 
     return scaled_mask
 
@@ -38,11 +33,13 @@ def main():
     list_keys = list(masks.keys())
     print(np.array(masks[list(masks.keys())[0]]).shape)
 
-    with open(point_cloud_data_path, 'rb') as f:
-        point_cloud_data = pickle.load(f)
+    # with open(point_cloud_data_path, 'rb') as f:
+    #     point_cloud_data = pickle.load(f)
 
-    print(np.array(point_cloud_data).shape)
-    projected_image_height, projected_image_width = np.array(point_cloud_data).shape
+    # print(np.array(point_cloud_data).shape)
+    # projected_image_height, projected_image_width = np.array(point_cloud_data).shape
+    projected_image_height, projected_image_width = cv2.imread(point_cloud_data_path).shape[:2]
+    print(projected_image_height, projected_image_width)
 
     scaled_mask = {}
 
@@ -51,6 +48,7 @@ def main():
 
     with open(scaled_mask_path, 'wb') as f:
         pickle.dump(scaled_mask, f)
+
 
 
 if __name__ == "__main__":
