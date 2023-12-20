ef63c973912df77aa20179311f1192b64c49c614
Daniel Gros <danielmgros@gmail.com>
2023-12-20 11:00:59 -0600

    add precision, recall, overlap

--- 05-get-mask/validation/validate_get_mask.sh
+++ 05-get-mask/validation/validate_get_mask.sh
@@ -16,6 +16,14 @@
 #     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/processed/masks_2023-03-04_08.txt" \
 #     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/black_and_white_"
 
+# python3 "./mask_to_black_and_white_image.py" \
+#     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/processed/masks_2023-01-31_08.txt" \
+#     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/black_and_white_"
+    
 python3 "./mask_to_black_and_white_image.py" \
-    "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/processed/masks_projected_image_2023-01-30_08.00.00_png_ultra.txt" \
-    "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/black_and_white_"
+"/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/processed/masks_projected_image_2023-01-30_08.00.00_png_ultra.txt" \
+"/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/black_and_white_"
+
+# python3 "./mask_to_black_and_white_image.py" \
+# "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/scaled_mask_2023-01-30_08.00.00_png_ultra.txt" \
+# "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/05-get-mask/data/validation/black_and_white_"
--- 06-scale-mask/validation/validate_get_mask.sh
+++ 06-scale-mask/validation/validate_get_mask.sh
@@ -24,3 +24,8 @@ python3 "./mask_to_color_image.py" \
     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/processed/scaled_mask_2023-01-30_08.00.00_png_ultra.txt" \
     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/raw/projected_image_2023-01-30_08.00.00_png_ultra.jpg" \
     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/validation/color_"
+
+# python3 "./mask_to_color_image.py" \
+#     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/raw/masks_projected_image_2023-01-30_08.00.00_png_ultra.txt" \
+#     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/raw/projected_image_2023-01-30_08.00.00_png_ultra.jpg" \
+#     "/hdd/gros2/maskrcnn/samples/plants_to_leaf_area/06-scale-mask/data/validation/color_"
